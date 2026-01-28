526ce751e9c20e8e823f4a39a5a9c0bd36db5bda
Naman Bhugra <namanbhugra7@gmail.com>
2026-01-28 12:33:45 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const client = net.createConnection({ port: 4000 }, () => {
+  console.log("Connected to server");
+});
+
+client.on("data", (data) => {
+  process.stdout.write(data.toString());
+});
+
+client.on("close", () => {
+  console.log("\nDisconnected from server");
+});
+
+process.stdin.on("data", (data) => {
+  client.write(data);
+});
